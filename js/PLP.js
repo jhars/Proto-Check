f4080b7a39dfdcfb285965fa1ed74eeeec2a0c5a
Jon Harlan <jon.harlan@thredup.com>
2016-06-06 18:02:03 -0700

    Dropped In Items

---
+++ js/PLP.js
@@ -0,0 +1,13 @@
+var d = document.getElementById('black-dress'),
+    n = document.getElementById('black-dress-atc');
+
+n.addEventListener('mouseover', hideDiv, true);
+n.addEventListener('mouseout', showDiv, true);
+
+function hideDiv(e) {
+  d.className = 'hide';
+}
+
+function showDiv(e) {
+ d.className = 'show';
+}

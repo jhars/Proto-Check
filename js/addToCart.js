6cd6a098160162cbb3ac6952e836291fce9604c9
JONATHAN HARLAN <jonathanharlan@JONATHANs-MacBook-Pro.local>
2016-06-08 05:33:03 -0700

    Popup Firing with Base Layout

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,21 +1,48 @@
-// window.addEventListener('load', initControl, false);
-document.addEventListener('mouseover', function() {
-		console.log("event listener Added!")
-		drawCart()
+var dress = document.getElementById('dress');
+var blouse = document.getElementById('blouse');
+var jeans = document.getElementById('jeans');
+
+var ctxDress = dress.getContext('2d');
+var ctxBlouse = blouse.getContext('2d');
+var ctxJeans = jeans.getContext('2d');	
+
+$(window).load(function(){
+  console.log('load');
+//drawScene() ??
+  	loadImages(sources, function(images) {
+		ctxDress.drawImage(images.dress, 0, 0, 249, 413);
+		ctxBlouse.drawImage(images.blouse, 25, 0, 249, 413);
+		ctxJeans.drawImage(images.jeans, 25, 0, 249, 413);
 	});
+});
 
-var canvasItems = document.getElementById('dress');
-var contextItems = canvasItems.getContext('2d');
+dress.addEventListener('mouseover', function() {
+	drawCartDress();
+});
+blouse.addEventListener('mouseover', function() {
+	drawCartBlouse();
+});
+jeans.addEventListener('mouseover', function() {
+	drawCartJeans();
+});
+dress.addEventListener('mouseout', function() {
+	drawDress();
+});
+blouse.addEventListener('mouseout', function() {
+	drawBlouse();
+});
+jeans.addEventListener('mouseout', function() {
+	drawJeans();
+});
 
-
-
-sources = {
-	dress: "./img/custom/black_dress.jpg"
-}
+var sources = {
+	dress: "./img/custom/black_dress.jpg",
+	blouse: "./img/custom/blouse.jpg",
+	jeans: "./img/custom/jeans.jpg"
+};
 
 var images = {};
 function loadImages(sources, callback) {
-
   var loadedImages = 0;
   var numImages = 0;
   for(var src in sources) {
@@ -29,24 +56,34 @@ function loadImages(sources, callback) {
     };
     images[src].src = sources[src];
   }
-}
-
-
-
-function initControl() {
-	canvasItems.addEventListener('mouseover', function() {
-		console.log("event listener Added!")
-		drawCart()
-	});
-	loadImages(sources, function(images) {
-		contextItems.drawImage(images.dress, 0, 0, 249, 413);
-	});
 };
-	
-function drawCart() {
+
+function drawDress() {
+		ctxDress.drawImage(images.dress, 0, 0, 249, 413);
+};
+
+function drawBlouse() {
+		ctxBlouse.drawImage(images.blouse, 25, 0, 249, 413);
+};
+
+function drawJeans() {
+		ctxJeans.drawImage(images.jeans, 25, 0, 249, 413);
+};
+
+function drawCartDress() {
 	var atc = new Image();
 	atc.src = "./img/custom/AddToCart.png"
-	contextItems.drawImage(atc, 150,390, 97, 19);
-}
+	ctxDress.drawImage(atc, 150,390, 97, 19);
+};
 
+function drawCartBlouse() {
+	var atc = new Image();
+	atc.src = "./img/custom/AddToCart.png"
+	ctxBlouse.drawImage(atc, 150,390, 97, 19);
+};
 
+function drawCartJeans() {
+	var atc = new Image();
+	atc.src = "./img/custom/AddToCart.png"
+	ctxJeans.drawImage(atc, 150,390, 97, 19);
+};
---
+++ js/popup.js
@@ -0,0 +1,52 @@
+var YMAL = document.getElementById('you-may-also-like');
+var dressPopup = document.getElementById('dress-popup');
+var btnCheckout = document.getElementById('btn-checkout');
+var btnContinue = document.getElementById('btn-continue');
+
+var ctxYMAL = YMAL.getContext('2d');
+var ctxDressPopup = dressPopup.getContext('2d');
+var ctxBtnCheckout = btnCheckout.getContext('2d');
+var ctxBtnContinue = btnContinue.getContext('2d');
+
+
+
+$(window).load(function(){
+  console.log('load');
+  	loadPopupImages(sources, function(images) {
+		ctxYMAL.drawImage(imagesPopup.you_may_also_like, 0,0,827,319);
+		ctxDressPopup.drawImage(imagesPopup.dress_popup, 0,0,137,183);
+		ctxBtnCheckout.drawImage(imagesPopup.checkout, 0,0,448,60);
+		ctxBtnContinue.drawImage(imagesPopup.continue, 0,0,448,60);
+	});
+});
+
+var sourcesPopup = {
+	dress_popup: "./img/custom/black_dress-popup.png",
+	you_may_also_like: "./img/custom/YouMayAlsoLike.png",
+	checkout: "./img/custom/Checkout-Now.png",
+	continue: "./img/custom/Continue-Shopping.png"
+};
+
+var imagesPopup = {};
+function loadPopupImages(sources, callback) {
+  var loadedImages = 0;
+  var numImages = 0;
+  for(var src in sourcesPopup) {
+    numImages++;
+  } for (var src in sourcesPopup) {
+    imagesPopup[src] = new Image();
+    imagesPopup[src].onload = function() {
+      if(++loadedImages >= numImages) {
+        callback(imagesPopup);
+      }
+    };
+    imagesPopup[src].src = sourcesPopup[src];
+  }
+};
+
+
+// function youMayAlsoLike() {
+// 	ctxYMAL.drawImage(imagesPopup.youMayAlsoLike, 0, 0, 827, 319);
+// 	console.log("loaded  You May Also Like Image");
+// };
+
